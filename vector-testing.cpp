62c66d4df8036e60905b15769a24c24be23e5dc4
stley <57237098+stley@users.noreply.github.com>
2026-02-19 07:19:44 -0300

    Added support for non-trivial types, such as classes and structs

--- vector-testing.cpp
+++ vector-testing.cpp
@@ -7,14 +7,32 @@ berkeley::vector<float> Floats;
 berkeley::vector<double> Doubles;
 berkeley::vector<double*> Pointers;
 
-struct Person
+class Person
 {
-    std::string name;
-    int age = 0;
-    bool isCool = true;
+public:
+    Person(std::string name, int age, bool isCool) :
+    name_(name),
+    age_(age),
+    isCool_(isCool)
+    {
+    }
+
+    std::string name_;
+    int age_ = 0;
+    bool isCool_ = true;
+    
 };
 
-//berkeley::vector<Person> People;
+
+struct DataTypes
+{
+    std::string TypeName = "nil";
+    size_t byte_size = 0;
+};
+
+berkeley::vector<Person> People;
+
+berkeley::vector<DataTypes> Types;
 
 void printLine(const char* fmt, ...)
 {
@@ -32,9 +50,7 @@ int main()
     //printLine("Vector size: %d elements (%d bytes) | Capacity: %d elements (%d bytes)", Integers.size(), Integers.byte_size(), Integers.capacity(), Integers.byte_cap());
 
     //size_t vector_size = Integers.capacity();
-    Integers.resize(5);
-    Floats.resize(5);
-    Doubles.resize(5);
+    
     for (int i = 0; i < 10; i++)
     {
         Integers.push_back(i+3);
@@ -48,6 +64,10 @@ int main()
     Floats.push_back(0.67);
     Doubles.push_back(4.20);
     Pointers.push_back( (Doubles.data()+(Doubles.size()-1) ));
+
+    Integers.resize(5);
+    Floats.resize(5);
+    Doubles.resize(5);
     for (int i = 0; i < Integers.size(); i++)
     {
         std::cout << "at " << i << " " << (Integers.at(i)) << "\n";
@@ -72,7 +92,42 @@ int main()
     printLine("\"double*\" Vector size: %d elements (%d bytes) | Capacity: %d elements (%d bytes)", Doubles.size(), Doubles.byte_size(), Doubles.capacity(), Doubles.byte_cap());
     
     
-
+    printLine("Object testing");
     
+    People.push_back(Person("Berkeley", 19, true));
+    People.push_back(Person("Lulo", 20, true));
+    People.push_back(Person("Messi", 38, true));
+
+
+    for(size_t i = 0; i < People.size(); i++)
+    {
+        printLine("At index %d: My name is %s, I'm %d years old and %s", i, (People.at(i)).name_.c_str(), (People.at(i)).age_, ( ( People.at(i) ).isCool_) ? "I'm cool :)" : "I'm not cool :(");
+    }
+
+    printLine("\"Person\" Vector size: %d elements (%d bytes) | Capacity: %d elements (%d bytes)", People.size(), People.byte_size(), People.capacity(), People.byte_cap());
+
+    DataTypes Type1;
+    DataTypes Type2;
+    DataTypes Type3;
+    DataTypes Type4;
+
+    Type1.TypeName = "Integer";
+    Type1.byte_size = sizeof(int);
+    Type2.TypeName = "Char";
+    Type2.byte_size = sizeof(char);
+    Type3.TypeName = "Double";
+    Type3.byte_size = sizeof(double);
+
+    Types.push_back(Type1);
+    Types.push_back(Type2);
+    Types.push_back(Type3);
+    Types.push_back(Type4);
+
+    printLine("Data Types:");
+    for(size_t i = 0; i < Types.size(); i++)
+    {
+        printLine("%s - Bytes: %d", (Types.at(i)).TypeName.c_str(), (Types.at(i)).byte_size);
+    }
+
     return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -17,11 +17,11 @@ private:
     size_t size_;
     size_t capacity_;
 
-
+    bool grow();
 public:
     vector()
     {
-        static_assert(std::is_trivially_copyable_v<T>, "Vector for non trivial instances is not supported.");
+        //static_assert(std::is_trivially_copyable_v<T>, "Vector for non trivial instances is not supported.");
         this->data_pointer = nullptr;
         this->size_ = 0;
         this->capacity_ = 0;
@@ -30,9 +30,9 @@ public:
     {
         for (size_t i = 0; i < size_; i++)
         {
-            //(data_pointer+i)->~T();
+            (data_pointer+i)->~T();
         }
-        free(data_pointer);
+        ::operator delete(data_pointer);
     }
     
     //Element Access
@@ -40,15 +40,19 @@ public:
     T& operator[](size_t index);
     const T& at(size_t index) const;
     const T& operator[](size_t index) const;
-    T& front() { return data_pointer; }
-    T& back() { return (data_pointer + size_); }
+    T& front() { return *data_pointer; }
+    T& back() 
+    {
+        if ( size_ < 1) return *data_pointer;
+        return *(data_pointer + (size_-1));
+    }
     T* data() { return data_pointer; }
     
     //Capacity
     bool empty() const { return (size_ == 0); }
     size_t size() const { return this->size_; }
     size_t byte_size() const { return (this->size_)*sizeof(T); }
-    size_t max_size() const { return this->capacity_; }
+    size_t max_size() const { return return SIZE_MAX / sizeof(T); }
     size_t capacity() const { return this->capacity_; }
     size_t byte_cap() const { return (this->capacity_)*(sizeof(T)); }
     
@@ -68,6 +72,33 @@ public:
 
 
 
+template <typename T>
+bool vector<T>::grow()
+{    
+    std::cout << "Grow called\n";
+    size_t new_cap = capacity_ + (capacity_/2);
+    if(!capacity_) new_cap = 2;
+    
+    T* new_ptr = static_cast<T*>(::operator new( (sizeof(T)*new_cap) ));
+
+    if (!new_ptr) return false;
+
+    for(size_t i = 0; i < size_; i++)
+    {
+        new (new_ptr + i) T(std::move( *(data_pointer + i) ));
+    }
+
+    for(size_t i = 0; i < size_; i++)
+    {
+        data_pointer[i].~T();
+    }
+
+    ::operator delete(data_pointer);
+
+    data_pointer = new_ptr;
+    capacity_ = new_cap;
+    return true;
+}
 
 template <typename T>
 T& vector<T>::at(size_t index)
@@ -102,36 +133,24 @@ const T& vector<T>::operator[](size_t index) const
 template <typename T>
 void vector<T>::clear()
 {
+    for(size_t i = 0; i < size_; i++)
+    {
+        data_pointer[i].~T();
+    }
+
     this->size_ = 0;
 }
 
 template <typename T>
 bool vector<T>::push_back(const T& value)
 {
-    if (!data_pointer)
-    {
-        capacity_ = 2;
-        data_pointer = reinterpret_cast<T*>(malloc(sizeof(T)*capacity_));
-        if (!data_pointer) return false;
-        
-        
-    }
-    else
-    {
-        if (size_ == capacity_)
-        {
-            size_t new_cap = capacity_ + (capacity_/2);
-            T* new_ptr = reinterpret_cast<T*>(realloc(data_pointer, (sizeof(T)*new_cap) ));
 
-            if (!new_ptr)
-                return false;
-            
-            capacity_ = new_cap;
-            data_pointer = new_ptr;
-        }
+    if(size_ == capacity_)
+    {
+        if (!this->grow())
+            return false;
 
     }
-
     new (data_pointer + size_) T(value);
     size_++;
     return true;
@@ -141,28 +160,90 @@ template <typename T>
 void vector<T>::pop_back()
 {
     if(size_ > 0)
-    size_--;
+    {
+
+        data_pointer[size_-1].~T();
+        size_--;
+    }
 }
 
 template <typename T>
 bool vector<T>::resize(size_t newsize)
 {
-    if(!data_pointer)
+    if (!data_pointer)
     {
-        data_pointer = reinterpret_cast<T*>(malloc(sizeof(T)*newsize));
+        if (newsize == 0)
+            return true;
+        data_pointer = static_cast<T*>( ::operator new (sizeof(T)*newsize));
         if (!data_pointer) return false;
         capacity_ = newsize;
+
+        for (size_t i = 0; i < newsize; ++i)
+        {
+            new (data_pointer + i) T();
+        }
+
+        size_ = newsize;
         return true;
     }
-    T* reallocatedPointer = reinterpret_cast<T*>(realloc(data_pointer, (sizeof(T)*newsize)));
-    if(!reallocatedPointer) return false;
 
-    data_pointer = reallocatedPointer;
-    if(newsize < size_)
-        capacity_ = newsize;
+    size_t old_size = size_;
 
+    if (newsize == 0)
+    {
+        this->clear();
+        return true;
+    }
+    
+    if (newsize <= size_)
+    {
+        for(size_t i = newsize; i < size_; i++)
+        {
+            data_pointer[i].~T();
+        }
+        size_ = newsize;
+        return true;
+    }
+
+    
+    if (newsize <= capacity_)
+    {
+        for(size_t i = old_size; i < newsize; i++)
+        {
+            new (data_pointer + i) T();
+        }
+    }
+
+    else
+    {
+        T* reallocatedPointer = static_cast<T*>( ::operator new ( (sizeof(T) * newsize)) );
+
+        if(!reallocatedPointer)
+            return false;
+
+        
+
+        for(size_t i = 0; i < size_; i++)
+        {
+            new (reallocatedPointer + i) T(std::move( *(data_pointer + i) ));
+        }
+
+        for(size_t i = 0; i < old_size; i++)
+        {
+            data_pointer[i].~T();
+        }
+
+        for (size_t i = old_size; i < newsize; ++i)
+        {
+            new (reallocatedPointer + i) T();
+        }
+
+        ::operator delete(data_pointer);
+
+        data_pointer = reallocatedPointer;
+        capacity_= newsize;
+    }
     size_ = newsize;
-
     return true;
 }
 
@@ -171,23 +252,35 @@ bool vector<T>::reserve(size_t to_reserve)
 {
     if (!data_pointer)
     {
-        data_pointer = reinterpret_cast<T*>(malloc(sizeof(T)*to_reserve));
+        data_pointer = static_cast<T*>( ::operator new(sizeof(T)*to_reserve) );
         if (!data_pointer) return false;
         capacity_ = to_reserve;
         return true;
     }
 
-    size_t new_cap = capacity_+to_reserve;
+    if (capacity_ >= to_reserve)
+        return true;
 
-    T* reallocatedPointer = reinterpret_cast<T*>(realloc(data_pointer, (sizeof(T)*new_cap)));
-    if(!reallocatedPointer) return false;
+    T* reallocatedPointer = static_cast<T*>( ::operator new( (sizeof(T)*to_reserve)) );
+
+    for(size_t i = 0; i < size_; i++)
+    {
+        new (reallocatedPointer + i) T(std::move( *(data_pointer + i) ));
+    }
+    for(size_t i = 0; i < size_; i++)
+    {
+        data_pointer[i].~T();
+    }
+
+    if (!reallocatedPointer)
+        return false;
+
+    ::operator delete(data_pointer);
 
     data_pointer = reallocatedPointer;
-    capacity_ = new_cap;
+    capacity_ = to_reserve;
     return true;
 }
 
 
 }
-
-
